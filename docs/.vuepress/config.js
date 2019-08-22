7bdb3337599f4246ee7eab9a26c01f8f5adce264
teacc <teacc@MarlondeiMac.local>
2019-08-22 16:46:08 +0800

    blog init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'Teacc Blog',
+    description: 'Just playing around',
+    // webpack 别名
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@alias': ''
+            }
+        }
+    }
+}
